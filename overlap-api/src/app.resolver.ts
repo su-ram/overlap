33ab9ee5a08a14cc4d5bf195244cae5370d76e03
김수람 <suram@gimsulam-ui-MacBookPro-2.local>
2025-12-12 23:37:01 +0900

    api 서버 삭제

--- overlap-api/src/app.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class AppResolver {
-  @Query(() => String, { description: 'Simple health check for the API' })
-  hello(): string {
-    return 'Hello from overlap-api GraphQL server';
-  }
-}
